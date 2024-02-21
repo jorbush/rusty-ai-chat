3067e761553eacd60784096e891448af01f6a518
Jordi <55429631+jorbush@users.noreply.github.com>
2024-02-21 09:04:00 +0100

    Define labels and history format

--- src/api.rs
+++ src/api.rs
@@ -8,4 +8,30 @@ pub async fn converse(prompt: Conversation) -> Result<String, ServerFnError> {
     use leptos_actix::extract;
     use actix_web::web::Data;
     use actix_web::web::ConnectionInfo;
+
+    let model = extract(|data: Data<Llama>, _connection: ConnectionInfo| async {
+        data.into_inner();
+    }).await.unwrap();
+
+    use llm::KnownModel;
+    let character_name = "### Assistant";
+    let user_name = "### Human";
+    let persona = "A chat between a human and an AI assistant.";
+    let history = format!(
+        "{CHARACTER_NAME}:Hello - How may I help you today?\n\
+        {USER_NAME}:What is the capital of France?\n\
+        {CHARACTER_NAME}:Paris is the capital of France.\n"
+    );
+
+    for message in prompt.messages.into_iter() {
+        let msg = message.text;
+        let current_line = if message.user {
+            format!("{character_name}:{msg}\n")
+        } else {
+            format!("{user_name}:{msg}\n")
+        };
+        history.push_str(&current_line);
+    }
+
+    Ok(String::from(""))
 }
