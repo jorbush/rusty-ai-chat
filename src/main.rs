78c9b5d6b45187e741992939b50cee8fa7594b94
Jordi <55429631+jorbush@users.noreply.github.com>
2024-03-15 09:49:44 +0100

    Use model and load css

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
+use cfg_if::cfg_if;
+
+pub mod api;
+pub mod model;
+
 #[cfg(feature = "ssr")]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
     use actix_files::Files;
     use actix_web::*;
     use leptos::*;
     use leptos_actix::{generate_route_list, LeptosRoutes};
-    use rusty_ai_chat::app::*;
+    use rusty_llama::app::*;
 
     let conf = get_configuration(None).await.unwrap();
     let addr = conf.leptos_options.site_addr;
     // Generate the list of routes in your Leptos App
-    let routes = generate_route_list(App);
-    println!("listening on http://{}", &addr);
+    let routes = generate_route_list(|| view! { <App/> });
 
+    #[get("/style.css")]
+    async fn css() -> impl Responder {
+        actix_files::NamedFile::open_async("./style/output.css").await
+    }
+
+    let model = web::Data::new(get_language_model());
     HttpServer::new(move || {
         let leptos_options = &conf.leptos_options;
         let site_root = &leptos_options.site_root;
 
         App::new()
-            // serve JS/WASM/CSS from `pkg`
-            .service(Files::new("/pkg", format!("{site_root}/pkg")))
-            // serve other assets from the `assets` directory
-            .service(Files::new("/assets", site_root))
-            // serve the favicon from /favicon.ico
-            .service(favicon)
-            .leptos_routes(leptos_options.to_owned(), routes.to_owned(), App)
-            .app_data(web::Data::new(leptos_options.to_owned()))
-        //.wrap(middleware::Compress::default())
+            .app_data(model.clone())
+            .service(css)
+            .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
+            .leptos_routes(
+                leptos_options.to_owned(),
+                routes.to_owned(),
+                || view! { <App/> },
+            )
+            .service(Files::new("/", site_root))
     })
     .bind(&addr)?
     .run()
