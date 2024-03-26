d7256e56a766a0f2f08bf91abb2a6fd8027e05ef
Jordi <55429631+jorbush@users.noreply.github.com>
2024-03-26 19:56:53 +0100

    Add tailwind classes and import

--- src/app/components/chat_area.rs
+++ src/app/components/chat_area.rs
@@ -1,5 +1,10 @@
 use::leptos::*;
 
+use crate::model::conversation::Conversation;
+
+const USER_MESSAGE_CLASS: &str = "max-w-md p-4 mb-5 rounded-lg self-end bg-blue-500 text-white";
+const AI_MESSAGE_CLASS: &str = "max-w-md p-4 mb-5 rounded-lg self-start bg-gray-200 text-black";
+
 #[component]
 pub fn ChatArea(conversation: ReadSignal<Conversation>) -> impl IntoView {
     view! {
