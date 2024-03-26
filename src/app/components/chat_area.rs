77f9b20e71c3ab6a8172d79ec7729ad4ce45b4a0
Jordi <55429631+jorbush@users.noreply.github.com>
2024-03-26 20:07:07 +0100

    Add automatic scroll effect when a new message is added

--- src/app/components/chat_area.rs
+++ src/app/components/chat_area.rs
@@ -7,6 +7,17 @@ const AI_MESSAGE_CLASS: &str = "max-w-md p-4 mb-5 rounded-lg self-start bg-gray-
 
 #[component]
 pub fn ChatArea(conversation: ReadSignal<Conversation>) -> impl IntoView {
+    let chat_div_ref = create_node_ref::<leptos::html::Input>();
+
+    create_effect(move |_| {
+        // scroll to the bottom of the chat area
+        // when a new message is added
+        conversation.get(); // subscribe to the signal
+        if let Some(chat_div) = chat_div_ref.get() {
+            chat_div.set_scroll_top(chat_div.scroll_height() as i32);
+        }
+    });
+
     view! {
         <div>
             {move || conversation.get().messages.iter().map(move |message| {
